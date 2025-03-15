8318897f25e4e7a4f4abc66728bef15596bd62a8
Mohamed Yusri <mohamedyusri205@gmail.com>
2025-03-15 13:09:23 +0200

    token created successfully

--- .history/src/Assignment7_20250315120904.sol
+++ .history/src/Assignment7_20250315120904.sol
@@ -2,21 +2,20 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Assignment7 is ERC20,Ownable {
+contract Assignment7 is ERC20 {
     // Constructor to initialize the token
     // Fill in the parameters for the token name and symbol
+    constructor(string memory _name,string memory _symbol) ERC20(_name,_symbol) {
 
-    constructor(string memory _name,string memory _symbol) ERC20(_name,_symbol) Ownable(msg.sender) {
+        
     }
 
     // Function to mint tokens
     // Fill in the visibility modifier to be external
     // Set params as to address and amount
-    function mint(address to,uint256 amount) external onlyOwner{
+    function mint() {
         // Fill in the logic
         // call _mint function with to and amount params
-        _mint(to,amount);
     }
 }
--- .history/src/Assignment7_20250315124943.sol
+++ .history/src/Assignment7_20250315124943.sol
@@ -2,21 +2,21 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol"
 
 contract Assignment7 is ERC20,Ownable {
     // Constructor to initialize the token
     // Fill in the parameters for the token name and symbol
 
-    constructor(string memory _name,string memory _symbol) ERC20(_name,_symbol) Ownable(msg.sender) {
+    constructor(string memory _name,string memory _symbol) ERC20(_name,_symbol) {
     }
 
     // Function to mint tokens
     // Fill in the visibility modifier to be external
     // Set params as to address and amount
-    function mint(address to,uint256 amount) external onlyOwner{
+    function mint(unit256 amount) public onlyOwner{
         // Fill in the logic
         // call _mint function with to and amount params
-        _mint(to,amount);
+        _mint(to,amount)
     }
 }
